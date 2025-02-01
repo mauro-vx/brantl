152ea73bec28a1d227798f7aaa651edaf66c47fb
Mauro Vio <maurovio.dev@gmail.com>
2025-02-01 13:44:22 +0100

    chore: setup Tailwind, font-family typo and basic style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,17 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
   subsets: ["latin", "latin-ext"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
   title: "Brantl - Pragmatické poradenství pro moderní firmy",
-  description:
-    "Brantl poskytuje poradenství zaměřené na inovace, růst a efektivitu moderních firem.",
+  description: "Brantl poskytuje poradenství zaměřené na inovace, růst a efektivitu moderních firem.",
   openGraph: {
     title: "Brantl - Pragmatické poradenství pro moderní firmy",
-    description:
-      "Pragmatické poradenství zaměřené na pomoc moderním firmám s růstem a inovacemi.",
+    description: "Pragmatické poradenství zaměřené na pomoc moderním firmám s růstem a inovacemi.",
     url: "https://brantl.cz",
-  },
-  icons: {
-    icon: "/favicon.ico",
   },
 };
 
@@ -29,14 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        <div className="flex flex-col min-h-screen max-w-6xl mx-auto px-4">
-          <header className="sticky top-0 z-50 bg-white shadow">
-            <div className="py-4">Header Content</div>
-          </header>
-          <main>{children}</main>
-          <footer className="py-4 border-t">Footer Content</footer>
-        </div>
+      <body className={`${montserrat.className} antialiased container max-w-full`}>
+        <header className="sticky top-0 z-50 bg-white shadow">Header Content</header>
+
+        <main>{children}</main>
+
+        <footer className="py-4 border-t">Footer Content</footer>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
   },
   plugins: [],
